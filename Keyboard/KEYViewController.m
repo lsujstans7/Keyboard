669eb91902911c13c610a27c879610fb8c8f52ac
lsujstans7 <jstans7@lsu.edu>
2012-08-02 15:06:24 -0500

    Final Keyboard

--- Keyboard/KEYViewController.m
+++ Keyboard/KEYViewController.m
@@ -10,46 +10,23 @@
 
 @interface KEYViewController ()
 
-@property (nonatomic, strong) AVAudioPlayer *playerC;
-@property (nonatomic, strong) AVAudioPlayer *playerD;
-@property (nonatomic, strong) AVAudioPlayer *playerE;
-@property (nonatomic, strong) AVAudioPlayer *playerF;
-@property (nonatomic, strong) AVAudioPlayer *playerG;
-@property (nonatomic, strong) AVAudioPlayer *playerA;
-@property (nonatomic, strong) AVAudioPlayer *playerB;
-@property (nonatomic, strong) AVAudioPlayer *playerOctave;
-
+@property (nonatomic, strong) AVAudioPlayer *player;
 @end
 
 @implementation KEYViewController
-@synthesize playerC;
-@synthesize playerD;
-@synthesize playerE;
-@synthesize playerF;
-@synthesize playerG;
-@synthesize playerA;
-@synthesize playerB;
-@synthesize playerOctave;
+@synthesize player;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    NSURL *fileURLC = [[NSURL alloc] initFileURLWithPath:[[NSBundle mainBundle]pathForResource:@"C" ofType:@"m4a"]];
+    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:[[NSBundle mainBundle]pathForResource:@"C" ofType:@"m4a"]];
     
-    self.playerC = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURLC error:nil];
-    if(self.playerC)
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+    if(self.player)
     {
-        playerC.numberOfLoops = 0;
+        player.numberOfLoops = 0;
     }
-    NSURL *fileURLD = [[NSURL alloc] initFileURLWithPath:[[NSBundle mainBundle]pathForResource:@"D" ofType:@"m4a"]];
-    
-    self.playerD = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURLD error:nil];
-    if(self.playerD)
-    {
-        playerD.numberOfLoops = 0;
-    }
-
 }
 
 - (void)viewDidUnload
@@ -67,58 +44,21 @@
     }
 }
 
-- (IBAction)playNoteC:(UIButton *)sender {
-    playerC.currentTime = 0;
-    playerC.play;
+- (IBAction)playNote:(UIButton *)sender {
+    NSURL *fileURL = [[NSURL alloc] initFileURLWithPath:[[NSBundle mainBundle]pathForResource:sender.titleLabel.text ofType:@"m4a"]];
+    
+    self.player = [[AVAudioPlayer alloc] initWithContentsOfURL:fileURL error:nil];
+    if(self.player)
+    {
+        player.numberOfLoops = 0;
+    }
+
+    player.currentTime = 0;
+    player.play;
 }
 
-- (IBAction)stopNoteC:(UIButton *)sender {
-    playerC.stop;
-}
-
-- (IBAction)playNoteD:(UIButton *)sender {
-    playerD.currentTime = 0;
-    playerD.play;
-}
-
-- (IBAction)stopNoteD:(UIButton *)sender {
-    playerD.stop;
-}
-
-- (IBAction)playNoteE:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteE:(UIButton *)sender {
-}
-
-- (IBAction)playNoteF:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteF:(UIButton *)sender {
-}
-
-- (IBAction)playNoteG:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteG:(UIButton *)sender {
-}
-
-- (IBAction)playNoteA:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteA:(UIButton *)sender {
-}
-
-- (IBAction)playNoteB:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteB:(UIButton *)sender {
-}
-
-- (IBAction)playNoteOctave:(UIButton *)sender {
-}
-
-- (IBAction)stopNoteOctave:(UIButton *)sender {
+- (IBAction)stopNote:(UIButton *)sender {
+    player.stop;
 }
 
 
