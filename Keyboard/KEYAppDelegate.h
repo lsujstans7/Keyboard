d58839129f637c1000b6ccc95cc99f63360bf04c
lsujstans7 <jstans7@lsu.edu>
2012-08-02 10:44:30 -0500

    Initial Commit

---
+++ Keyboard/KEYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KEYAppDelegate.h
+//  Keyboard
+//
+//  Created by iOS Camp on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KEYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Keyboard/KEYViewController.h
@@ -0,0 +1,13 @@
+//
+//  KEYViewController.h
+//  Keyboard
+//
+//  Created by iOS Camp on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KEYViewController : UIViewController
+
+@end
